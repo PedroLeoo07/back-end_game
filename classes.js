2d0a5eef65fa2fc0378ecfdfaa40a27dccfab736
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2024-11-14 11:44:39 -0300

    Feat: Classe de impostor criada

--- classes.js
+++ classes.js
@@ -13,3 +13,17 @@ class Jogador {
 }
 
 export default Jogador
+
+
+class Impostor {
+    constructor(nome, id, equipe) {
+        this.id = this.generate()
+        this.nome = nome,
+        this.id = id,
+        this.equipe = equipe
+    }
+
+    generateId() {
+        return Math.floor(Math.random() * 999) + 1
+    }
+}
